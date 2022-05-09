27f7cb8d70e11c9924b9245af1eb8d2dedda286b
Tovintsoa <tianatovintsoa@gmail.com>
2022-05-09 22:23:51 +0300

    try server

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+app.get('/', (request, response) => {
+    console.log(`URL: ${request.url}`);
+    response.send('Hello, Server!');
+});
